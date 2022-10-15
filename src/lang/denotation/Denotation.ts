abff96c19b6d4d0ca2e13ef135490eecbd4554c2
Xie Yuheng <xyheme@gmail.com>
2022-10-16 06:03:27 +0800

    up

--- src/lang/denotation/Denotation.ts
+++ src/lang/denotation/Denotation.ts
@@ -1,1 +1,21 @@
-export type Denotation = {}
+export type Denotation = Fn | Let | Ctor | Data
+
+export type Fn = {
+  family: "Denotation"
+  kind: "Fn"
+}
+
+export type Let = {
+  family: "Denotation"
+  kind: "Let"
+}
+
+export type Ctor = {
+  family: "Denotation"
+  kind: "Ctor"
+}
+
+export type Data = {
+  family: "Denotation"
+  kind: "Data"
+}
