f6045d86feedba9e185b1395120e1cb61130155f
Xie Yuheng <xyheme@gmail.com>
2022-10-25 09:26:22 +0800

    up

--- src/lang/termination/OrderMatrix.ts
+++ src/lang/termination/OrderMatrix.ts
@@ -2,18 +2,18 @@ import { Matrix, Ring, Vector } from "./Matrix"
 import * as Orders from "./Order"
 import { Order } from "./Order"
 
-export class OrderMatrix extends Matrix<Order> {
-  static ring: Ring<Order> = {
-    add: Orders.addOrder,
-    mul: Orders.mulOrder,
-    zero: Orders.LargerOrNotComparable,
-    one: Orders.Neutral,
-  }
+const orderRing: Ring<Order> = {
+  add: Orders.addOrder,
+  mul: Orders.mulOrder,
+  zero: Orders.LargerOrNotComparable,
+  one: Orders.Neutral,
+}
 
+export class OrderMatrix extends Matrix<Order> {
   static fromRows(rows: Array<Array<Order>>): OrderMatrix {
     return new Matrix(
-      this.ring,
-      rows.map((row) => new Vector(this.ring, row)),
+      orderRing,
+      rows.map((row) => new Vector(orderRing, row)),
     )
   }
 }
