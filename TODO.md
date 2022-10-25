aeac0da2b2e4c242f181ef27497132c47764f5f9
Xie Yuheng <xyheme@gmail.com>
2022-10-25 09:25:09 +0800

    CallMatrix.compose

--- TODO.md
+++ TODO.md
@@ -1,4 +1,3 @@
-composeCallMatrix
 completeCallMatrixes
 
 # later
--- src/lang/termination/CallMatrix.ts
+++ src/lang/termination/CallMatrix.ts
@@ -33,4 +33,23 @@ export class CallMatrix {
 
     return new CallMatrix(left, OrderMatrix.fromRows(rows), right)
   }
+
+  composable(that: CallMatrix): boolean {
+    return this.right === that.left
+  }
+  compose(that: CallMatrix): CallMatrix {
+    if (!this.composable(that)) {
+      throw new Error(
+        [
+          `CallMatrix not composable`,
+          `  this.left: ${this.left}`,
+          `  this.right: ${this.right}`,
+          `  that.left: ${that.left}`,
+          `  that.right: ${that.right}`,
+        ].join("\n"),
+      )
+    }
+
+    return new CallMatrix(this.left, this.matrix.mul(that.matrix), that.right)
+  }
 }
