89e16294f7c3f60b761334799790c3619c78065b
Xie Yuheng <xyheme@gmail.com>
2022-10-27 19:34:39 +0800

    up

--- src/lang/termination/Order.ts
+++ src/lang/termination/Order.ts
@@ -32,26 +32,22 @@ export function addOrder(x: Order, y: Order): Order {
 
 export function minOrder(x: Order, y: Order): Order {
   if (x === Smaller && y === Smaller) return Smaller
-  if (x === Smaller && y === Neutral) return Smaller
-  if (x === Neutral && y === Smaller) return Smaller
+  if (x === Smaller && y === Neutral) return Neutral
+  if (x === Neutral && y === Smaller) return Neutral
   if (x === Neutral && y === Neutral) return Neutral
   return LargerOrNotComparable
 }
-
-// TODO Why the paper uses the following definition?
-
-// export function minOrder(x: Order, y: Order): Order {
-//   if (x === Smaller && y === Smaller) return Smaller
-//   if (x === Smaller && y === Neutral) return Neutral
-//   if (x === Neutral && y === Smaller) return Neutral
-//   if (x === Neutral && y === Neutral) return Neutral
-//   return LargerOrNotComparable
-// }
 
 export function maxOrders(xs: Array<Order>): Order {
   return xs.reduce((result, x) => addOrder(result, x), LargerOrNotComparable)
 }
 
 export function minOrders(xs: Array<Order>): Order {
-  return xs.reduce((result, x) => minOrder(result, x), Neutral)
+  return xs.reduce((result, x) => mulOrder(result, x), Neutral)
+}
+
+// TODO Why the paper uses minOrder the following definition?
+
+export function minOrdersFromThePaper(xs: Array<Order>): Order {
+  return xs.reduce((result, x) => minOrder(result, x), Smaller)
 }
